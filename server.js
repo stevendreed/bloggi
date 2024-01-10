60dc5f82f69ccb87202a4ae652e638ce8a34f0a8
stevendreed <steven.daniel.reed@gmail.com>
2024-01-09 18:53:46 -0800

    added boiler plate connective middleware for express server, json, req.params, using routes, handlebar tempaltes, & starting sequelize ORM

--- server.js
+++ server.js
@@ -11,3 +11,39 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: 'default',
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 6, // 6 hours
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict'
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// included so blog posts can be saved as json
+app.use(express.json());
+
+// included to access req.params
+app.use(express.urlendcoded({ extended: true }));
+
+// set static served pages path to ${REPOSITORY}/public
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+// initialize sequelize ORM 
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
+});

4629f1bc8f7a03471c341167b94737d226eccc36
stevendreed <steven.daniel.reed@gmail.com>
2024-01-09 19:06:39 -0800

    rolled back some boiler plate middleware to debug server, added connection.js boiler plate for sequelize

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require ('express');
 const session = require('express-session');
 const exphbs = require ('express-handlebars');
 const routes = require('./controllers');
-const dotenv = require('dotenv');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -12,36 +11,36 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-const sess = {
-    secret: 'default',
-    cookie: {
-        maxAge: 1000 * 60 * 60 * 6, // 6 hours
-        httpOnly: true,
-        secure: false,
-        sameSite: 'strict'
-    },
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
-};
+// const sess = {
+//     secret: 'default',
+//     cookie: {
+//         maxAge: 1000 * 60 * 60 * 6, // 6 hours
+//         httpOnly: true,
+//         secure: false,
+//         sameSite: 'strict'
+//     },
+//     resave: false,
+//     saveUninitialized: true,
+//     store: new SequelizeStore({
+//         db: sequelize
+//     })
+// };
 
-app.use(session(sess));
+// app.use(session(sess));
 
-app.engine('handlebars', hbs.engine);
+app.engine('handlebars', exphbs.engine);
 app.set('view engine', 'handlebars');
 
 // included so blog posts can be saved as json
 app.use(express.json());
 
 // included to access req.params
-app.use(express.urlendcoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 // set static served pages path to ${REPOSITORY}/public
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+// app.use(routes);
 
 // initialize sequelize ORM 
 sequelize.sync({ force: false }).then(() => {

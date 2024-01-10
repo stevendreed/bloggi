e981db38df37ce2e457a8f6229164df324ea8578
stevendreed <steven.daniel.reed@gmail.com>
2024-01-10 13:22:59 -0800

    added password

--- model/User.js
+++ model/User.js
@@ -4,3 +4,25 @@
 
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+
+class User extends Model {}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allownull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8]
+            }
+        },
+        user_email:
+        {}
+    }
+)
